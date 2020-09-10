a58ba9ee21534afdc4f79e5238a0d2ff8f6cf092
Muhammad Kashif <44mkashif@gmail.com>
2020-09-10 14:26:12 +0500

    Created login/ports stats pages

--- app.py
+++ app.py
@@ -25,5 +25,13 @@ def access_switch1():
 def access_switch2():
     return render_template('access_switch2.html')
 
+@app.route('/login_stats')
+def login_stats():
+    return render_template('login_stats.html')
+
+@app.route('/ports_stats')
+def ports_stats():
+    return render_template('ports_stats.html')
+
 if __name__ == "__main__":
     app.run(debug=True, host='172.30.211.14')
